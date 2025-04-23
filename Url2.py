748e8184713cdaace58e2f6c31d46daa5b8f695a
KisForPotasuim <udie.is.gud@gmail.com>
2025-04-23 02:26:15 +0000

    Pending changes exported from your codespace

--- Url2.py
+++ Url2.py
@@ -1,6 +1,7 @@
 import requests
 import base64
 from requests.auth import HTTPBasicAuth
+import json
 
 
 # https://services.onetcenter.org/ws/mnm/occupations?keyword=engineer&start=1&end=5
@@ -11,8 +12,9 @@ credentials = HTTPBasicAuth(onet_username, onet_password)
 # credentials_2 = base64.b64encode(credentials.encode()).decode()
 # headers = {"Authorization": f"{credentials_2}"}
 
+
 class Url():
-    def __init__(self,type,category,subcat,sortcat,keyword,prep,initial=21,end=40,code=None):
+    def __init__(self,type,category,subcat,sortcat,keyword,prep,initial=1,end=2,code=None):
         self.baseUrl="https://services.onetcenter.org/ws/"
         self.service=type
         self.category=category
@@ -25,6 +27,7 @@ class Url():
         self.end=end
         self.code=code
         self.sortUrl()
+        # self.careerInfo=self.get_onet_careers()
 
     def decode(self):
         if(not self.subcat):
@@ -47,6 +50,8 @@ class Url():
             flag=True
         elif(self.category=="occ"):
             url=url+str(self.code)+str(self.fineTuning[self.category][""])
+        elif(self.category=="default" and self.code):
+            url+=self.code
         else:
             url=url+temp
         return url, flag
@@ -54,7 +59,7 @@ class Url():
     def sortUrl(self):
         url=self.handleUrl()
         url=url[0]
-        if(self.category=="occ"):
+        if(self.category=="default"):
             pass
         else:
             # flag=url[1]
@@ -63,6 +68,53 @@ class Url():
             url=url+self.sortcatSplit[self.sort]+end
         self.url=url
         return url #for debugging i guess
+
+    def get_onet_careers(self,headers=credentials):
+        self.careers=[]
+        accept={"Accept" : "application/json"}
+        response = requests.get(self.url, auth=headers, headers=accept)
+
+        if response.status_code == 200:
+            data = response.json()
+            careers = []
+            for job in data.get(self.jsonKey[self.category], []):
+                job_code = job["code"]
+                job_title = job["title"]
+                temp=Url("default","default","","",a,5,code=job_code)
+                job_info = self.get_onet_job_details(temp)
+                self.careers.append((job_title, job_info))
+            # return data
+            return self.careers if self.careers else [("test", "")]
+        return [("Failed to fetch data from O*NET", "")]
+
+    def get_onet_job_details(self,temp,headers=credentials):
+        # https://services.onetcenter.org/v1.9/ws/mnm/careers/[O*NET-SOC Code]/job_outlook("salary")
+
+        accept={"Accept" : "application/json"}
+        accessList=[temp.url,temp.url+self.detailFindKey["salary"],temp.url+self.detailFindKey["school"]] #these are most important:general description,salary+brightoutlook, and prep needed
+        file=None
+        tag={"0":"Description:","1":"Salary:","2":"Prep:","3":"Education:"}
+        for i in accessList:
+            response = requests.get(i, auth=headers, headers=accept)
+            
+            if response.status_code == 200:
+                data = response.json()
+                tempList=[
+                data.get("what_they_do", None),
+                data.get("salary", {}).get("annual_median", None),
+                data.get("job_zone",None),
+                data.get("education_usually_needed",{}).get("category",None)]
+                if(not file):
+                    file=open(f"{data.get("title","data")}.txt","w+") #this works, but too much, so I change sort val
+                
+                for j in range(len(tempList)):
+                    if not tempList[j]==None:
+                        file.write(tag[str(j)]+"\n"+str(tempList[j])+"\n")
+        file.close()
+            # return f"Description: {description}\nMedian Salary: ${salary}\n"
+        #     return "Details unavailable." #422 error code
+
+
 
     categorySplit={"default":"careers/","future":"bright_outlook/","prep":"job_preparation/","search":"search/","occ":"occupation/"}
     serviceSplit={"default":"mnm/","web":"online/"}
@@ -74,59 +126,10 @@ class Url():
             "search":{"keyword":"?keyword="},
             "occ":{"":"/summary"}
             }
-    sortcatSplit={"name":"?sort=name","future":"?sort=bright_outlook","search":""}
-    sortsplit={"def":"&start=2&end=4"} #chars index is 7,13
-
-def get_onet_careers(url,headers=credentials):
-    response = requests.get(url.url, auth=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        careers = []
-        for job in data.get("occupation_list", []):
-            url=Url("web","occ","","name",a,5,code=job_code)
-            job_code = job["code"]
-            job_title = job["title"]
-            job_info = get_onet_job_details(url)
-            careers.append((job_title, job_info))
-        return careers if careers else [("No relevant careers found", "")]
-    return [("Failed to fetch data from O*NET", "")]
-
-# def get_onet_job_details(job_code,headers=headers):
-#     url = f"{onet_url}/occupation/{job_code}/summary"
-#     response = requests.get(url, headers=headers)
-#     if response.status_code == 200:
-#         data = response.json()
-#         description = data.get("description", "No description available.")
-#         wage_info = data.get("wages", {}).get("national_median", {})
-#         salary = wage_info.get("annual", "Salary data unavailable")
-#         return f"Description: {description}\nMedian Salary: ${salary}"
-#     return "Details unavailable."
-
-
-# def get_onet_careers(url,headers=headers):
-#     response = requests.get(url.url, headers=headers)
-#     if response.status_code == 200:
-#         data = response.json()
-#         careers = []
-#         for job in data.get("occupation_list", []):
-#             job_code = job["code"]
-#             job_title = job["title"]
-#             # job_info = get_onet_job_details(job_code)
-#             # careers.append((job_title, job_info))
-#         return job_code
-#         return careers if careers else [("No relevant careers found", "")]
-#     return [("Failed to fetch data from O*NET", "")]
-
-def get_onet_job_details(url,headers=credentials):
-    response = requests.get(url.url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        description = data.get("description", "No description available.")
-        wage_info = data.get("wages", {}).get("national_median", {})
-        salary = wage_info.get("annual", "Salary data unavailable")
-        return f"Description: {description}\nMedian Salary: ${salary}"
-    return "Details unavailable."
+    sortcatSplit={"name":"?sort=name","future":"?sort=bright_outlook","search":"","":""}
+    sortsplit={"def":"&start=1&end=2","":""} #chars index is 7,13
+    jsonKey={"default":"career","future":"career","prep":"career","search":"occupation","occ":"occupation"}
+    detailFindKey={"salary":"/job_outlook","prep":"/knowledge","skills":"/skills","school":"/education"}
 
 
 a="artichect"
@@ -134,10 +137,11 @@ test=[
 Url("default","future","grow","name",a,5),
 Url("web","search","keyword","search",a,5),
 Url("default","prep","ready","future",a,5),
-Url("default","default","","name",a,5),
-Url("web","occ","","name",a,5,code="51-9191.00")
-] #0,2,3 are good..
+Url("default","default","","",a,5,code="17-2071.00"),
+] #0,1,2,3 are good..
 
-for i in test:
-    print(i.url)
-    print(get_onet_careers(i,headers=credentials))
+# for i in test:
+#     print(i.url)
+
+print(test[0].get_onet_careers())
+# print(Url("default","default","","",a,5,code="17-2071.00").url)
